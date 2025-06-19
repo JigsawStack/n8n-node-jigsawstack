f83388ad758eca6d49e9c99a892facafe4a812ef
Vineet Agarwal <91052168+VineeTagarwaL-code@users.noreply.github.com>
2025-06-19 23:50:35 +0530

    feat: added auth and test in creds

--- credentials/JigsawStackApi.credentials.ts
+++ credentials/JigsawStackApi.credentials.ts
@@ -1,12 +1,15 @@
 import {
-  ICredentialType,
-  INodeProperties,
+	IAuthenticateGeneric,
+	ICredentialTestRequest,
+	ICredentialType,
+	INodeProperties,
 } from 'n8n-workflow';
 
 export class JigsawStackApi implements ICredentialType {
   name = 'jigsawStackApi';
   displayName = 'JigsawStack API';
   documentationUrl = 'https://jigsawstack.com/docs';
+
   properties: INodeProperties[] = [
     {
       displayName: 'API Key',
@@ -19,4 +22,27 @@ export class JigsawStackApi implements ICredentialType {
       required: true,
     },
   ];
-} 
+
+  authenticate: IAuthenticateGeneric = {
+		type: 'generic',
+		properties: {
+			headers: {
+				'x-api-key': '={{$credentials.apiKey}}',
+				accept: 'application/json',
+				'Content-Type': 'application/json',
+			},
+		},
+	};
+
+  test: ICredentialTestRequest = {
+    request: {
+      method: 'POST',
+      url: 'https://api.jigsawstack.com/v1/ai/translate',
+      body: {
+        text: ['Hello'],
+        target_language: 'zh',
+      },
+      json: true,
+    },
+  };
+}
